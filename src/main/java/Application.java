55e7fa5a9d06973f2aeaf67f0d46c3b3c9ba501f
venugopalmurugan <61824073+venugopalmurugan@users.noreply.github.com>
2022-11-15 11:51:48 +0530

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+
+private int b=90;
+    public static void main(String[] args){
+        int a =10;
+        int b = 20;
+        Application app = null;
+        System.out.println(app.b);
+    }
+}
